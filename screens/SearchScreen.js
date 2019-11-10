03f227c93940879cdf3684c425425b299425dc35
Lillian Gales <lilliangales@ymail.com>
2019-11-09 20:19:44 -0800

    connected to the backend

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Text } from 'react-native';
 import GallerySwiper from "react-native-gallery-swiper";
+import superagent from 'superagent';
+
+const API = process.env.REACT_APP_API; 
 
 export default function FavoritesScreen() {
 
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+    fetch('http://localhost:3000/search/Dog/98103/15', options)
+    .then((results) => results.json())
+    .then((data) => console.log(data))
+  })
+
+
   return (
+
     <GallerySwiper
       images={[
         { url: "https://loremflickr.com/1080/1920/dog" },
