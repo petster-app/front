c785bf32c7a1624545ac31b83969b58ce0c0809d
Lillian Gales <lilliangales@ymail.com>
2019-11-16 00:41:53 -0800

    added icons and modularized the navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen'
 import InputScreen from '../screens/InputScreen'
 import SearchScreen from '../screens/SearchScreen'
-import FavoritesScreen from '../screens/FavoritesScreen';;
-
-const TabNavigator = createBottomTabNavigator({
-  HomeScreen: HomeScreen,
-  InputScreen: InputScreen,
-  FavoritesScreen: FavoritesScreen,
-  SearchScreen: SearchScreen,
-});
+import FavoritesScreen from '../screens/FavoritesScreen';
+import TabNavigator from './TabNavigator';
 
 const AppNavigator = createStackNavigator({
   HomeScreen: HomeScreen,
--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import InputScreen from '../screens/InputScreen'
+import SearchScreen from '../screens/SearchScreen'
+import FavoritesScreen from '../screens/FavoritesScreen';;
 
-import HomeScreen from '../screens/HomeScreen';
-import InputScreen from '../screens/InputScreen';
-import FavoritesScreen from '../screens/FavoritesScreen';
-
-export default createAppContainer(
-  createBottomTabNavigator({
-    HomeScreen: HomeScreen,
-    InputScreen: InputScreen,
-    FavoritesScreen: FavoritesScreen,
-  }),
-);
+export default createBottomTabNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+      ),
+    }
+  },
+  InputScreen: {
+    screen: InputScreen,
+    navigationOptions: {
+      tabBarLabel: 'Input',
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+      ),
+    }
+  },
+  FavoritesScreen: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarLabel: 'Favorites',
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+          focused={focused}
+          name={
+            Platform.OS === 'ios'
+              ? `ios-information-circle${focused ? '' : '-outline'}`
+              : 'md-options'
+          }
+        />
+      ),
+    }
+  },
+  SearchScreen: {
+    screen: SearchScreen,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+          focused={focused}
+          name={
+            Platform.OS === 'ios'
+              ? `ios-information-circle${focused ? '' : '-outline'}`
+              : 'md-information-circle'
+          }
+        />
+      ),
+    }
+  },
+});
