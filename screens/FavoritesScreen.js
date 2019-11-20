31406b84f4ea7454400dac213b4e3015eac158bc
Lillian Gales <lilliangales@ymail.com>
2019-11-19 21:53:48 -0800

    rendering favorites from database for mock user

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,34 +1,49 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, Slider} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {StyleSheet, Text, View, Image } from 'react-native';
+import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery';
 
-export default function slider () {
-  const [travelDistance, setTravelDistance] = useState(5);
+export default function favoritesScreen () {
+  const [favorites, setFavorites] = useState([]);
 
-  function handleSlider(travelDistance) {
-    setTravelDistance(travelDistance)
-  }
+  let user = 'Bob'
 
+  useEffect(() => {
+    let options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+    fetch(`http://localhost:3000/favorites/${user}`, options)
+      .then((results) => results.json())
+      .then((data) => {
+        console.log(data)
+        setFavorites(data);
+      })
+  }, [])
+
+    console.log(favorites)
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>{String ('Search within ' + travelDistance + ' miles')}</Text>
-        <Slider
-          step={5}
-          maximumValue={100}
-          onValueChange={handleSlider}
-          value={travelDistance}
-        />
+        <Text>Favorite Pets</Text>
+        {favorites.map((pet, index) => 
+        <View key={index}>
+          <Image style={{width: 200, height: 200}} source={{uri : pet.photo }} />
+          <Text style={styles.text}>{pet.name}</Text>
+          <Text style={styles.text}>{pet.age}</Text>
+          <Text style={styles.text}>{pet.description}</Text>
+        </View>
+        )}
       </View>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
   text: {
-    fontSize: 35,
+    fontSize: 15,
     textAlign: 'center',
   },
+  image: {
+    textAlign: 'center'
+  }
 });
