5d5f0efc5bf272d71b273c53e630b055b7e5d672
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-02-25 13:04:50 -0800

    login setup for homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
-import { AuthSession } from 'expo';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput, View, Button }  from 'react-native';
+
 
 export default function HomeScreen(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  function handleSubmit(event) {
-    props.navigation.navigate('InputScreen');
+  function handleLogin () {
+    // TODO: Firebase stuff...
+    console.log('handleLogin')
   }
 
   return (
@@ -13,12 +17,37 @@ export default function HomeScreen(props) {
       <Text style={styles.text}>
         Welcome to petster!
       </Text>
+      <Text style={styles.bodyText}>Sign In</Text>
+      {errorMessage &&
+      <Text style={{ color: 'red' }}>
+        {errorMessage}
+      </Text>}
+      <TextInput
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Email"
+        onChangeText={email => setEmail(email)}
+        value={email}
+      />
+      <TextInput
+        secureTextEntry
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Password"
+        onChangeText={password => setPassword(password)}
+        value={password}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Button
+        title="Don't have an account? Sign Up"
+        onPress={() => this.props.navigation.navigate('SignUp')}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { 
+  container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
@@ -31,5 +60,21 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontFamily: 'AmaticSC-Bold',
   },
+  bodyText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#ffffff',
+    fontFamily: 'AmaticSC-Bold',
+  },
+  textInput: {
+    height: 40,
+    width: '90%',
+    marginLeft: '5%',
+    borderColor: 'white',
+    borderWidth: 1,
+    marginTop: 8,
+    textAlign: 'center',
+    color: '#ffffff',
+  },
 });
-
